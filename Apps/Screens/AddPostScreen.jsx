aa2438a4c4198c6ed8716ee65e5e188501dd2db9
Arnab765 <842arnab@gmail.com>
2024-04-11 23:41:04 +0530

    form modifications
    validation, effects, etc

--- Apps/Screens/AddPostScreen.jsx
+++ Apps/Screens/AddPostScreen.jsx
@@ -1,13 +1,15 @@
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image, ToastAndroid } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { app } from '../../firebaseConfig';
 import { Formik } from 'formik';
 import { Picker } from '@react-native-picker/picker';
+import * as ImagePicker from 'expo-image-picker';
 import { getFirestore, getDocs, collection } from 'firebase/firestore';
 
 
 export default function AddPostScreen() {
   
+  const [image, setImage] = useState(null);
   const db = getFirestore(app);
   const [categoryList, setCategoryList]=useState([]);
 
@@ -25,17 +27,60 @@ export default function AddPostScreen() {
     })
 
   }
+
+  /**
+   * Used to pick image from gallery
+   */
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   
+  const onSubmitMethod=(value)=>{
+    value.image=image;
+    console.log(value);
+  }
+
   return (
     <View className="p-10">
       <Text className="text-[27px] font-bold">Add New Post</Text>
       <Text className="text-[16px] text-gray-500 mb-7">Create New Post and Start Selling</Text>
       <Formik
         initialValues={{title:'', desc:'', category:'', address:'', price:'', image:''}}
-        onSubmit={value=>console.log(value)}
-        >
-          {({handleChange,handleBlur,handleSubmit,values})=>(
+        onSubmit={value=>onSubmitMethod=(value)}
+        validate={(values)=>{
+          const errors={}
+          if(!values.title)
+          {
+            console.log("Title not Present");
+            ToastAndroid.show('Title must be there', ToastAndroid.SHORT)
+            errors.name="Title must be there"
+          }
+          return errors
+        }}
+      >
+          {({handleChange,handleBlur,handleSubmit,values,setFieldValue,initialErrors})=>(
             <View>
+
+              <TouchableOpacity onPress={pickImage}/>
+              {image?
+              <image source={{uri:image}} style={{width:100,height:100,borderRadius:15}}/>
+              :
+              <Image source={require('./../../assets/images.jpeg')}
+              style={{width:100,height:100,borderRadius:15}}
+                />}
                 <TextInput
                   style={styles.input}
                   placeholder='Title'
@@ -76,8 +121,8 @@ export default function AddPostScreen() {
               </Picker>
 
                 <TouchableOpacity onPress={handleSubmit} 
-                  className="p-5 bg-blue-500 rounded-full mt-20">
-                  <Text className="text-white text-center text-[16px]">Search</Text>
+                  className="p-5 bg-blue-500 rounded-full mt-3">
+                  <Text className="text-white text-center text-[17px]">Search</Text>
                 </TouchableOpacity>
 
 
