80cb0a42ceb8f9ebb4555b490064e22521e6c82b
Piyush <piyush_pandey_bca21@jimsindia.org>
2024-04-23 17:46:12 +0530

    search bar added

--- Apps/Screens/HomeScreen.jsx
+++ Apps/Screens/HomeScreen.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../Components/HomeScreen/Header'
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className='py-8 px-6 bg-white flex-1'>
+      <Header/>
     </View>
   )
 }
